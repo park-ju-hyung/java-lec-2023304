d0482976ffa6dc8d5452c063aca929d9b2b3e51f
park <ptg0131@gmail.com>
2023-04-28 22:01:38 +0900

    0428

---
+++ Day02/src/HelloWord.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class HelloWord {
+	public static void main(String args[]){
+		System.out.println("오늘은 2일차");
+		
+		
+		System.out.println();
+		
+		
+		
+		
+	}
+}
---
+++ Day02/src/JavaStructure.java
@@ -0,0 +1,20 @@
+// 컴파일 에러를 발생시키는 기초 문법
+//1. 자바는 클래스이름과 파일명이 같아야합니다. 
+//2. 자바는 대소문자를 구분합니다. 
+//EX) Java java -> 다르다
+//3. {} 중괄호로 문단, 문장을 구분합니다. 
+//4. 자바의 문장의 끝은 ;을 사용합니다
+
+
+
+
+public class JavaStructure {
+	public static void main(String args[]){
+		System.out.println("하악하악"); 
+		// 문장의  끝에는 ; 이 필요해서 오류.
+	}
+}
+
+
+
+
